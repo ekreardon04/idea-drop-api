1973404cca9566eec50bed0ca409f8f08aa0b460
ekreardon04 <ekreardon04@gmail.com>
2026-06-22 16:16:33 -0700

    added custo, error handler

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import ideaRouter from './routes/ideaRoutes.js'
+import { errorHandler } from './middleware/errorHandler.js'
 
 dotenv.config()
 
@@ -12,7 +13,19 @@ app.use(cors())
 app.use(express.json()) //middlewhere to accept raw json in req in body
 app.use(express.urlencoded({extended: true})) //urlencoded form data
 
+
+//ROUTES
 app.use('/api/ideas', ideaRouter)
+
+//404 Fallback
+app.use((req, res,next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`)
+
+    res.status(404)
+    next(error)
+})
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
