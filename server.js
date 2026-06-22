20deb479f59544145b6bb3a10d6c726ecd836fdc
ekreardon04 <ekreardon04@gmail.com>
2026-06-22 15:52:51 -0700

    added route files

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import ideaRouter from './routes/ideaRoutes.js'
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 8000
 
-app.use(cors)
+app.use(cors())
+app.use(express.json()) //middlewhere to accept raw json in req in body
+app.use(express.urlencoded({extended: true})) //urlencoded form data
+
+app.use('/api/ideas', ideaRouter)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
