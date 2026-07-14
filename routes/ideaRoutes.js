ba17e1646fec60a77a28e73574df6e6b0aaf802b
ekreardon04 <ekreardon04@gmail.com>
2026-07-14 14:36:33 -0700

    Prepfor deploy

--- routes/ideaRoutes.js
+++ routes/ideaRoutes.js
@@ -1,32 +1,171 @@
 import express from 'express'
+import Idea from '../models/Idea.js'
+import mongoose from 'mongoose'
+import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
+
 
 
 //@route               GET /api/ideas
 //@description         Get all ideas
 //@access              Public
-router.get('/', (req, res) => {
-    const ideas = [
-        {id: 1, title: 'Idea 1', description: 'This is idea 1'},
-        {id: 2, title: 'Idea 2', description: 'This is idea 2'},
-        {id: 3, title: 'Idea 3', description: 'This is idea 3'},
-    ]
+//@ query              _limit (optional limit for ideas returned)
+router.get('/', async (req, res, next) => {
+   try{
+      const limit = parseInt(req.query._limit)
+      const query = Idea.find().sort({createdAt: -1})
 
-  res.status(400)
-  throw new Error("This is an error")
+const ideas = await query.exec()
+res.json(ideas)
+   } catch(err){
+    console.log(err)
+next(err)
+   }
+})
 
-    res.json(ideas)
+//@route               GET /api/ideas/:id
+//@description         Get single idea
+//@access              Public
+router.get('/:id', async (req, res, next) => {
+   try{
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+    res.status(404)
+    throw new Error("Idea not found") 
+    }
+
+const idea = await Idea.findById(id)
+
+if(!idea){
+    res.status(404)
+    throw new Error("Idea not found")
+}
+res.json(idea)
+   } catch(err){
+    console.log(err)
+next(err)
+   }
 })
 
 //@route               POST /api/ideas
 //@description         Create new idea
 //@access              Public
 
-router.post('/', (req, res) => {
-   const {title, description} = req.body
-   console.log(description)
-    res.send(title)
+router.post('/', protect, async (req, res, next) => {
+  try{
+const {title, summary, description, tags } = req.body || {}
+
+if(!title?.trim() || !summary?.trim() || !description?.trim()){
+   res.status(404)
+   throw new Error(`Title, summary, and description are required`)
+}
+const newIdea = new Idea({
+   title,
+   summary,
+   description,
+   tags: typeof tags === 'string' ? 
+   tags.split(',')
+   .map((tag) => tag.trim())
+.filter(Boolean)
+: Array.isArray(tags) ? tags : [],
+user: req.user.id,
+})
+
+const savedIdea = await newIdea.save()
+res.status(201).json(savedIdea)
+  } catch(err) {
+   console.log(err)
+next(err)
+  }
+})
+
+//@route               DELETE /api/ideas/:id
+//@description        DELETE idea
+//@access              Public
+router.delete('/:id', protect, async (req, res, next) => {
+   try{
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+    res.status(404)
+    throw new Error("Idea not found") 
+    }
+
+const idea = await Idea.findById(id)
+if(!idea) {
+   res.status(404)
+   throw new Error("Idea not found")
+}
+
+//check if user owns idea
+if(idea.user.toString() !== req.user._id.toString()){
+   res.status(403)
+   throw new Error("Not authorized to delete this idea")
+}
+
+await idea.deleteOne()
+
+res.json({message: 'Idea deleted succesfully'})
+   } catch(err){
+    console.log(err)
+next(err)
+   }
+})
+
+
+////@route               PUT /api/ideas/:id
+//@description      Update idea
+//@access              Public
+router.put('/:id', protect, async (req,res,next) => {
+   try{
+const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+    res.status(404)
+    throw new Error("Idea not found") 
+    }
+
+const idea = await Idea.findById(id)
+if(!idea){
+   res.status(404)
+   throw new Error('Idea not found')
+}
+
+
+//check if user owns idea
+if(idea.user.toString() !== req.user._id.toString()){
+   res.status(403)
+   throw new Error("Not authorized to update this idea")
+}
+
+
+const {title, summary, description, tags} = req.body || {}
+
+if(!title?.trim() || !summary?.trim() || !description?.trim()){
+   res.status(404)
+   throw new Error(`Title, summary, and description are required`)
+}
+
+idea.title = title
+idea.summary= summary
+idea.description = description
+idea.tags= Array.isArray(tags) ? tags : typeof tags === 'string' ? 
+tags.split(',')
+.map((tag) => tag.trim())
+.filter(Boolean)
+: []
+
+const updatedIdea = await idea.save()
+
+
+res.json(updatedIdea)
+
+   } catch(err){
+      console.log(err)
+next(err)
+   }
 })
 
 
--- server.js
+++ server.js
@@ -2,20 +2,37 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import ideaRouter from './routes/ideaRoutes.js'
+import authRouter from './routes/authRoutes.js'
 import { errorHandler } from './middleware/errorHandler.js'
+import connectDB from './config/db.js'
+import cookieParser from 'cookie-parser'
+
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 8000
 
-app.use(cors())
+//Connnect to MongoDB
+connectDB()
+
+//cors config
+const allowedOrigins = [
+    'http://localhost:3000'
+]
+
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true
+}))
 app.use(express.json()) //middlewhere to accept raw json in req in body
 app.use(express.urlencoded({extended: true})) //urlencoded form data
+app.use(cookieParser())
 
 
 //ROUTES
 app.use('/api/ideas', ideaRouter)
+app.use('/api/auth', authRouter)
 
 //404 Fallback
 app.use((req, res,next) => {
